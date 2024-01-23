fae39591012e1c5bad1b5c4ad2f2cc180b0a74a5
Frank <0765655151@protonmail.ch>
2024-01-23 16:57:08 +0100

    modification classes boutons

--- front/src/App.svelte
+++ front/src/App.svelte
@@ -1,116 +1,87 @@
+<!-- Svelte script pour la navigation -->
 <script lang="ts">
   let url = "";
-  const navigate = (path: string) => (url = path);
-  import { onMount } from "svelte";
-  import interact from "interactjs";
+  const navigate = (path: string) => (url = path);  // Cette fonction modifie la variable 'url' en fonction du chemin passé
 </script>
 
-<div class="background">
-  <div class="cache"></div>
-  <div class="button-container">
-    <div class="buttonStart">
-      <button class="button1" on:click={() => navigate("/start")}>START</button>
+<!-- Structure HTML avec le fond et les boutons -->
+<div class="background">  <!-- Div pour le fond de la page -->
+
+    <!-- Bouton START -->
+    <div>
+      <button class="button1" on:click={() => navigate("/start")}>START</button>  <!-- Bouton START qui appelle la fonction navigate avec le chemin "/start" -->
     </div>
-    <div class="buttonLogs">
-      <button class="button" on:click={() => navigate("/login")}>Login</button>
-      <button class="button" on:click={() => navigate("/signin")}
-        >Sign In</button
-      >
+
+    <!-- Bouton Login -->
+    <div>
+      <button class="button2" on:click={() => navigate("/login")}>Login</button>  <!-- Bouton Login qui appelle la fonction navigate avec le chemin "/login" -->
     </div>
-  </div>
+
+    <!-- Bouton Sign In -->
+    <div>
+      <button class="button3" on:click={() => navigate("/signin")}>Sign In</button>  <!-- Bouton Sign In qui appelle la fonction navigate avec le chemin "/signin" -->
+    </div>
 </div>
 
+<!-- Styles CSS pour les éléments HTML -->
 <style>
   .background {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-image: url("./lib/homepage.jpg");
-    background-size: 100% 100%;
-    background-position: center;
+    /* Style pour le fond de la page */
+    position: absolute;  /* Positionnement absolu */
+    top: 0;  /* Haut de la page */
+    left: 0;  /* Gauche de la page */
+    width: 100%;  /* Largeur totale */
+    height: 100%;  /* Hauteur totale */
+    background-image: url("./lib/homepage.jpg");  /* Image de fond */
+    background-size: 100% 100%;  /* Taille de l'image de fond */
+    background-position: center;  /* Positionnement de l'image de fond */
   }
-  .button-container {
-    height: 20%;
-    width: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-  .button {
-    display: flex;
 
-    text-align: center;
-    vertical-align: middle;
-    padding: 14px 57px;
-    border: 7px solid #4e23db;
-    border-radius: 9px;
-    background: #4f24de;
-    background: -webkit-gradient(
-      linear,
-      left top,
-      left bottom,
-      from(#4f24de),
-      to(#4820c9)
-    );
-    background: -moz-linear-gradient(top, #4f24de, #4820c9);
-    background: linear-gradient(to bottom, #4f24de, #4820c9);
-    -webkit-box-shadow: #adacbf 0px 0px 0px 0px;
-    -moz-box-shadow: #adacbf 0px 0px 0px 0px;
-    box-shadow: #adacbf 0px 0px 0px 0px;
-    font: normal normal bold 26px arial;
-    color: #ffffff;
-    text-decoration: none;
-    text-transform: uppercase;
+  .button1 {
+    /* Style commun pour tous les boutons */
+    display: inline-block;  /* Affichage en bloc inline */
+    text-align: center;  /* Alignement du texte au centre */
+    vertical-align: middle;  /* Alignement vertical au milieu */
+    padding: 0.9% 7%;  /* Remplissage autour du texte */
+    border: 7px solid #2c87bb;  /* Bordure solide */
+    border-radius: 9px;  /* Rayon de la bordure arrondie */
+    background: #4f24de;  /* Couleur de fond */
+    font: normal normal bold 26px arial;  /* Style de police */
+    color: #ffffff;  /* Couleur de police */
+    text-decoration: none;  /* Pas de décoration du texte */
+    text-transform: uppercase;  /* Transformation du texte en majuscules */
+    transform: translateX(-10px) translateY(761px);
   }
-  .button:hover {
-    border: 7px solid #7d38ff;
-    background: #5f2bff;
-    background: -webkit-gradient(
-      linear,
-      left top,
-      left bottom,
-      from(#5f2bff),
-      to(#5626f1)
-    );
-    background: -moz-linear-gradient(top, #5f2bff, #5626f1);
-    background: linear-gradient(to bottom, #5f2bff, #5626f1);
-    color: #ffffff;
-    text-decoration: none;
-    text-transform: uppercase;
-    margin-bottom: 10px;
+  .button2 {
+    /* Style commun pour tous les boutons */
+    display: inline-block;  /* Affichage en bloc inline */
+    text-align: center;  /* Alignement du texte au centre */
+    vertical-align: middle;  /* Alignement vertical au milieu */
+    padding: 0.1px 5%;  /* Remplissage autour du texte */
+    border: 7px solid #2c87bb;  /* Bordure solide */
+    border-radius: 9px;  /* Rayon de la bordure arrondie */
+    background: #482aad;  /* Couleur de fond */
+    font: normal normal bold 26px arial;  /* Style de police */
+    color: #ffffff;  /* Couleur de police */
+    text-decoration: none;  /* Pas de décoration du texte */
+    text-transform: uppercase;  /* Transformation du texte en majuscules */
+    transform: translateX(-223px) translateY(821px);
+    }
+
+  .button3 {
+        /* Style commun pour tous les boutons */
+        display: inline-block;  /* Affichage en bloc inline */
+    text-align: center;  /* Alignement du texte au centre */
+    vertical-align: middle;  /* Alignement vertical au milieu */
+    padding: 0.1px 5%;  /* Remplissage autour du texte */
+    border: 7px solid #2c87bb;  /* Bordure solide */
+    border-radius: 9px;  /* Rayon de la bordure arrondie */
+    background: #4f24de;  /* Couleur de fond */
+    font: normal normal bold 26px arial;  /* Style de police */
+    color: #ffffff;  /* Couleur de police */
+    text-decoration: none;  /* Pas de décoration du texte */
+    text-transform: uppercase;  /* Transformation du texte en majuscules */
+    transform: translateX(225px) translateY(776px);
   }
-  .button:active {
-    background: #2f1685;
-    background: -webkit-gradient(
-      linear,
-      left top,
-      left bottom,
-      from(#2f1685),
-      to(#4820c9)
-    );
-    background: -moz-linear-gradient(top, #2f1685, #4820c9);
-    background: linear-gradient(to bottom, #2f1685, #4820c9);
-    text-transform: uppercase;
-  }
-  .button:focus {
-    text-transform: uppercase;
-  }
-  /*.button:before {
-    content: "\0000a0";
-    display: inline-block;
-    height: 24px;
-    width: 24px;
-    line-height: 24px;
-    margin: 0 4px -6px -4px;
-    position: relative;
-    top: 0px;
-    left: 0px;
-  }*/
-  .cache {
-    height: 75%;
-    width: 100%;
-   
-  }
+  
 </style>
