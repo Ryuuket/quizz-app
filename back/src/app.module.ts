e714d76758f5bff6d43fe17ebebd53b728ee525e
unknown <emma.broudic35@gmail.com>
2024-01-26 16:55:15 +0100

    type orm ok

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PlayerModule } from './player/player.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Player } from './player/player.entity';
@@ -10,8 +8,11 @@ import { Player } from './player/player.entity';
     PlayerModule,
     TypeOrmModule.forRoot({
         type: 'postgres',
-        host: 'localhost',
-        port: 5432,
+        host: process.env.DB_HOST || 'localhost',
+        port: parseInt(process.env.DB_PORT, 10) || 5432,
+        username: process.env.DB_USER || 'adminuser',
+        password: process.env.DB_PASSWORD || 'adminpassword',
+        database: process.env.DB_NAME || 'mydatabase',
         entities: [Player],
         synchronize: true,
         autoLoadEntities: true,
